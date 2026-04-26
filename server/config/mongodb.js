d5618d0fc2127dd5b6afdcfd11b1e5d51a76ef4f
asulekha <sulekhaa842@gmail.com>
2026-04-26 17:15:16 +0530

    Log MongoDB URI on connection
    Added logging for MongoDB URI during connection.

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -2,11 +2,13 @@ import mongoose from "mongoose"
 
 
 const connectDB = async () => {
+        console.log("MONGO URI:", process.env.MONGODB_URI)
     // next three line is event listner and ye record and ye jab chalta jab 4th line wala kam ho jata h 
     mongoose.connection.on('connected', () => {
         console.log("Database Connected")
     })
    await mongoose.connect(process.env.MONGODB_URI)
+    console.log("MONGO URI:", process.env.MONGODB_URI)
 }
 export default connectDB
 
